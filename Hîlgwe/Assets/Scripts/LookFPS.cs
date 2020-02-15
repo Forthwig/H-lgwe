b2db4465be8120bc71df5408453190c701a56e04
Dubois Thomas <thomas.dubois@isen.yncrea.fr>
2020-02-15 18:26:13 +0100

    Phase - 1 // Menu + FPS Controller
    Création d'un menu avec pour le moment 3 choix : Play / Options / Quit
    Création des scripts fps

---
+++ Hîlgwe/Assets/Scripts/LookFPS.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookFPS : MonoBehaviour
+{
+	public float mouseSensitivity = 100f;
+	public Transform playerBody;
+
+	float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      Cursor.lockState = CursorLockMode.Locked; //Hide the cursor  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime; //Because we are in void Update and we want our mouvement to be independant of frames
+    	float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+    	xRotation -= mouseY; //-= and not += because it will flip after ... 
+    	xRotation = Mathf.Clamp(xRotation, -90f,90f); //Restriction of rotation of the head (up/down)
+
+    	transform.localRotation = Quaternion.Euler(xRotation,0f,0f); //We are not using Rotate because we want to restrict movemenent
+
+    	playerBody.Rotate(Vector3.up*mouseX);
+
+        
+    }
+}
